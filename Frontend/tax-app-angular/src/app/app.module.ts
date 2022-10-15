8b98909b2ce3963a79ebc083c761d72f8ab63643
Karanja.Chiuri <karanjachiuri@gmail.com>
2022-10-15 20:42:08 +0300

    Imported our Standalone components. Routing module and HttpClient module.
    Added the router outlet html tag

--- Frontend/tax-app-angular/src/app/app.module.ts
+++ Frontend/tax-app-angular/src/app/app.module.ts
@@ -3,16 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CalculatorComponent } from './components/calculator/calculator.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+
+import { BillComponent } from './components/bill/bill.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CalculatorComponent,
+    BrowserAnimationsModule,
+
+    BillComponent,
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
