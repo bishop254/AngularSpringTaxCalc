06e429ff1d75cf5e3f321caf426e5133037127f4
Karanja.Chiuri <karanjachiuri@gmail.com>
2022-10-15 20:43:37 +0300

    Teams Page Component

---
+++ Frontend/tax-app-angular/src/app/components/teams/teams.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-teams',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './teams.component.html',
+  styleUrls: ['./teams.component.scss'],
+})
+export class TeamsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
